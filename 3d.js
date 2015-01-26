c61e0b9e94d8cd3647bcc9b98e4edba6fdff6e89
Aswini S <ashumeow@live.com>
2015-01-26 16:40:13 +0530

    Update 3d.js

--- 3d.js
+++ 3d.js
@@ -21,6 +21,51 @@ var meow3d = module.render3d = function(opts) {
 	var meow = this;
 	meow.domElement = xCanvas;
 
+	// Setting CSS 3D viewport size
+	meow.setSize = function() {
+		widthHalf3d = width3d / 2;
+		heightHalf3d = height3d / 2;
+	};
+
+	// Rendering the scenes using camera
+	// DOM renderers created especially for objects
+	meow.render = function(xScene, camera) {
+		var xObjects, object;
+		var x, xl;
+		var xVisible;
+		var xColor;
+		var xLineLength;
+		var xSVG;
+		var xRenderDOM;
+		xScene.updateMatrix();
+		syncScene(xScene);
+		updateCamera(camera);
+		xObjects = xScene.xObjects;
+		for(x = 0, xl = xObjects.length; x < xl; x++) {
+			object = xObjects[x];
+			xRenderDOM = object.xRenderDOM;
+
+			// Ignoring objects that have no updates
+			if(!xRenderDOM || !xRenderDOM.requireRender) {
+				continue;
+			}
+
+			// Applying visibility state
+			if(object.xVisible) {
+				if(!xRenderDOM.xVisible) {
+					xRenderDOM.show();
+				}
+			} else {
+				if(xRenderDOM.xVisible) {
+					xRenderDOM.hide();
+				}
+			}
+			// Updating objects marking it as done
+			xRenderDOM.update();
+			xRenderDOM.requireRender = false;
+		}
+	};
+
 	//
 	// Still more to code
 	//
