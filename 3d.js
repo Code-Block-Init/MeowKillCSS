8b9f442523c63a0d56552147528a5fc29b032d54
Aswini S <ashumeow@live.com>
2015-01-26 07:41:53 +0530

    Update 3d.js

--- 3d.js
+++ 3d.js
@@ -9,8 +9,18 @@ var meow3d = module.render3d = function(opts) {
 	var width3d, height3d;
 	var widthHalf3d, heightHalf3d;
 	var screenMatrix = new Meow3d.matrix();
-	screenObjectMatrix = new Meow3d.matrix();
-	
+	var screenObjectMatrix = new Meow3d.matrix();
+
+	// Preparing viewport and camera div
+	/* refer 3d.css to get clear idea */
+	var xCanvas = document.createElement('div');
+	var xCamera = document.createElement('div');
+	xCanvas.className = 'render-3d';
+	xCamera.className = 'camera-3d';
+	xCanvas.appendChild(xCamera);
+	var meow = this;
+	meow.domElement = xCanvas;
+
 	//
 	// Still more to code
 	//
