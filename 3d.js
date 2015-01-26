41b34abac2c1c435744c9cf2e22fa25533734555
Aswini S <ashumeow@live.com>
2015-01-26 07:33:16 +0530

    Update 3d.js

--- 3d.js
+++ 3d.js
@@ -1,9 +1,16 @@
 // CSS 3D rendering engine
 var meow3d = module.render3d = function(opts) {
 	'use strict';
+
 	// add parsing options
 	opts = opts || {};
 
+	// Preparing rendering state
+	var width3d, height3d;
+	var widthHalf3d, heightHalf3d;
+	var screenMatrix = new Meow3d.matrix();
+	screenObjectMatrix = new Meow3d.matrix();
+	
 	//
 	// Still more to code
 	//
