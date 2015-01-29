6a53c4ca2d9d67652fe5c315166ca71b6c91b875
Aswini S <ashumeow@live.com>
2015-01-29 12:51:10 +0530

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,5 +1,13 @@
 function meowLoaderCSS() {
 	"use strict";
+	var xx = window.document.createElement("link");
+	var ref = before || window.document.getElementsByTagName("script")[0];
+	var meowStyleSheets = window.document.styleSheets;
+	xx.rel = "stylesheet";
+	xx.href = href;
+	xx.media = "specific"; // fetches without blocking render
+	xx.onLoad = callback || function() {};
+	ref.parentNode.insertBefore(xx, ref);
 	//
 	// Still more to code!
 	//
